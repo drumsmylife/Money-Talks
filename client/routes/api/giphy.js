77f3165e654d9607d24e54a7eaab7e5ed432fb42
Sergio Rivera <thesurge7805@yahoo.com>
2019-05-07 20:53:22 -0500

    Updated Gif.js api requests

--- client/routes/api/giphy.js
+++ client/routes/api/giphy.js
@@ -1,5 +1,7 @@
-const giphyAPI = "JXkpKRAmyWWfdABgWzYBU3qlPkzbVD2q"
+// var axios = require("axios");
 
-var gif = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + giphyAPI + "&limit=1");
-gif.done(function(data) { console.log("success got data", data); });
+// const giphyAPI = "JXkpKRAmyWWfdABgWzYBU3qlPkzbVD2q"
+
+// axios.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + giphyAPI + "&limit=1");
+// gif.done(function(data) { console.log("success got data", data); });
 
