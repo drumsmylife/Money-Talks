339e9411c10384bdea75cdf0e10cdceaff83bc1d
Tommy Cervenka <drumsmylife@Tommys-MacBook-Pro.local>
2019-05-10 01:45:52 -0500

    update more code in current files/added new files plaid.js/Account.js/accountReducer/private-route/Accounts.js/accountActions/rearranged files out of components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,82 @@
 import React, { Component } from "react";
-import "./App.css";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+// import logo from "./components.NewLogo";
 import Gif from "./components/Gif";
 import Overview from "./components/Overview";
 import Signup from "./components/Signup";
 
-class App extends Component {
-  render() {
-    return(
-      <div className="container">
-        <span><Navbar/></span>
-        <Login/>
-        <Overview/>
-        <Signup/>
-        <br/><br/><br/>
-        <Gif/>
-      </div>
-    )
+import "./App.css";
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
   }
 }
-
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/Gif" component={Gif} />
+            <Route exact path="/Overview" component={Overview} />
+            <Route exact path="/Signup" component={Signup} />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
 export default App;
+
+
+// class App extends Component {
+//   render() {
+//     return(
+//       <div className="container">
+//         <span><Navbar/></span>
+//         <Login/>
+//         <Overview/>
+//         <Signup/>
+//         <br/><br/><br/>
+//         <Gif/>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
