b081d844098f3be45f0e3d151c6da66417f14443
Tommy Cervenka <drumsmylife@Tommys-MacBook-Pro.local>
2019-05-12 17:34:35 -0500

    Plaid API login/register fully accessible now API GOOD TO GO

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import store from "./store";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+import Login from "./components/auth/PlaidLogin";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 
