deb07fe76d32014d9e3dccfbb0a87bcfd7d3cf4b
Sergio Rivera <thesurge7805@yahoo.com>
2019-05-16 20:41:50 -0500

    frontend update

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+// import background from "../money.js";
+import "../layout/landing.css"; 
+
+
+
+
+
+
 
 class Landing extends Component {
   render() {
     return (
+      // <div className='background-image' style ={ {background: "url("+bg+")" } }></div>
+      // <img src={logo} alt="Money Talk$" style={{height:200, width:275,}}/>
+      // <ImageBackground source={background} style={{width: '100%', height: '100%'}}>
+      //   <Text>Inside</Text>
+      // </ImageBackground>
+  
+      
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
@@ -43,6 +58,8 @@ class Landing extends Component {
           </div>
         </div>
       </div>
+
+
     );
   }
 }
