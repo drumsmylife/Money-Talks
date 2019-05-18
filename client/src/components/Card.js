fd850581e4b648fa4db29a6dadb97a0a647310d8
Tommy Cervenka <tommy@cervenka.org>
2019-05-17 22:02:34 -0500

    messages for amounts are now showing in the cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,26 +8,26 @@ import React, {Component} from 'react';
 class Card extends Component{
     
     // Checks total from acounts.js and returns search term
-    // checkTotal(total){
-    //     if (total <= 500){
-    //         let giphySearchTerm = "Awesome"
-    //         console.log("less than 500", giphySearchTerm)
-    //         return giphySearchTerm
+    checkTotal(total){
+        if (total <= 500){
+            let giphySearchTerm = "Awesome"
+            console.log("less than 500", giphySearchTerm)
+            return giphySearchTerm
             
-    //     }
-    //     else if (500<= total <= 1500){
-    //         let giphySearchTerm = "Not+Bad"
-    //         return giphySearchTerm
-    //     }
-    //     else if (1500<= total <=5000){
-    //         let giphySearchTerm = "Slow+Down+Idiot"
-    //         return giphySearchTerm
-    //     }
-    //     else{
-    //         let giphySearchTerm = "STOP!"
-    //         return giphySearchTerm
-    //     }
-    // }
+        }
+        else if (500<= total && total <= 1500){
+            let giphySearchTerm = "Not Bad"
+            return giphySearchTerm
+        }
+        else if (1500<= total && total <=5000){
+            let giphySearchTerm = "Slow Down Idiot"
+            return giphySearchTerm
+        }
+        else{
+            let giphySearchTerm = "STOP!"
+            return giphySearchTerm
+        }
+    }
 
 
     
@@ -41,7 +41,8 @@ class Card extends Component{
                     {/* <img src={this.state.gifs} className="card-img-top" alt="Gif" /> */}
                     <div className="card-body">
                         <h4>Category: {this.props.category}</h4>
-                        <h5>Total Spent: $ {this.props.total}</h5>
+                        <h5>Total Spent:  $ {this.props.total}</h5>
+                        <h5>{this.checkTotal(this.props.total)}</h5>
                     </div>
                 </div>
             </div>
