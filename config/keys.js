e5401f66f0ecbe24f268285a03421ff442e8b9ee
Tommy Cervenka <tommy@cervenka.org>
2019-08-15 14:55:48 -0500

    more on the mongoURI added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://drumsmylife:Yeeyee182%21@moneytalks-nqjcx.mongodb.net/test?retryWrites=true",
+    mongoURI: "mongodb+srv://drumsmylife:Yeeyee182%21@moneytalks-nqjcx.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
