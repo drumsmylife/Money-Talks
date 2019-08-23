4dc564969150412425350ec401cd23ee5028606a
Tommy Cervenka <tommy@cervenka.org>
2019-08-23 17:34:59 -0500

    mongouri another fix

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//     mongoURI: "mongodb+srv://drumsmylife:YeeYee182@moneytalks-nqjcx.mongodb.net/test?retryWrites=true&w=majority",
+//     secretOrKey: "secret"
+//   };
+
 module.exports = {
-    mongoURI: "mongodb+srv://drumsmylife:YeeYee182@moneytalks-nqjcx.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-  };
+  mongoURI: "mongodb+srv://drumsmylife:yeeyee182@moneytalks-nqjcx.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
+};
