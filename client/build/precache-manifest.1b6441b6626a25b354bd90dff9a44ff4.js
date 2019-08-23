6cb103a9253da3a129246918a783f09f3a1a01c6
Tommy Cervenka <tommy@cervenka.org>
2019-08-23 17:16:18 -0500

    mongouri fixing

---
+++ client/build/precache-manifest.1b6441b6626a25b354bd90dff9a44ff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac9ff42c69beb4a842304d4efb93d6bd",
+    "url": "/static/media/money.ac9ff42c.jpg"
+  },
+  {
+    "revision": "4226549cf818a4ddfdd0a687b3cb72a0",
+    "url": "/static/media/NewLogo.4226549c.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3d261ea73ead22f6145",
+    "url": "/static/js/main.8606e4cd.chunk.js"
+  },
+  {
+    "revision": "d89367ba4a55aaf5d597",
+    "url": "/static/js/2.5ec75593.chunk.js"
+  },
+  {
+    "revision": "b3d261ea73ead22f6145",
+    "url": "/static/css/main.904b919d.chunk.css"
+  },
+  {
+    "revision": "cdabb5687f8b7d399953d48a822b79b6",
+    "url": "/index.html"
+  }
+];
